8baa4ad14070fbd1c6a4e427aca2d2301aabfca8
TARN <58160063@go.buu.ac.th>
2017-11-30 02:00:51 +0700

    edit

--- contact/index.js
+++ contact/index.js
@@ -9,14 +9,21 @@ let contactList = [
     {id: 05, firstname: 'Jora' , lastname: 'Mormont', Email: ' khaleesifan100@gmail.com', phone: '123-456-7890', url: 'www.google.com',  note: 'Lost in the friend-zone.'},
     {id: 06, firstname: 'Ned' , lastname: 'Stark', Email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', note: 'Winter is coming.'},
     {id: 07, firstname: 'Margaery' , lastname: 'Tyrell', Email: 'mtyrell@highgarden.com', phone: '123-456-7890', url: 'www.google.com', note: 'Keeper of kings.'},
-    {id: 09, firstname: 'Petyr' , lastname: 'Baelish', Email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', note: 'Do not trust anyone.'},
-    {id: 10, firstname: 'Samwell', lastname:  'Tarly', Email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', note: 'Loyal brother of the watch.'},
-    {id: 11, firstname: 'Stannis' , lastname: 'Baratheon', Email: 'onetrueking@dragonstone.com', phone: '123-456-7890', url: 'www.google.com', note: 'Nobody expects the Stannish inquisition.'},
-    {id: 12, firstname: 'Theon' , lastname: 'Greyjoy', Email: ' tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', note: 'Reluctant to pay iron price.'},
-    {id: 13, firstname: 'Tyrion' , lastname: 'Lannister', Email: 'tyrion@lannister.com', phone: '123-456-7890', url: 'www.google.com', note: 'Currently drunk.'}
+    {id: 08, firstname: 'Petyr' , lastname: 'Baelish', Email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', note: 'Do not trust anyone.'},
+    {id: 09, firstname: 'Samwell', lastname:  'Tarly', Email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', note: 'Loyal brother of the watch.'},
+    {id: 10, firstname: 'Stannis' , lastname: 'Baratheon', Email: 'onetrueking@dragonstone.com', phone: '123-456-7890', url: 'www.google.com', note: 'Nobody expects the Stannish inquisition.'},
+    {id: 11, firstname: 'Theon' , lastname: 'Greyjoy', Email: ' tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', note: 'Reluctant to pay iron price.'},
+    {id: 12, firstname: 'Tyrion' , lastname: 'Lannister', Email: 'tyrion@lannister.com', phone: '123-456-7890', url: 'www.google.com', note: 'Currently drunk.'}
 
     
 ]
+
+router.get('/contactList/:id', (req, res) => {
+    let id = req.params.id
+    let contact = contactList[id-1]
+    res.send({contact})
+  })
+
 
 //req(request), res(respond)
 router.get('/contactList', (req, res) =>{
