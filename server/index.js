e2eb67f3387267d5edf57361d6acf5ab6e6f8424
TARN <58160063@go.buu.ac.th>
2017-12-09 22:34:26 +0700

    Update Module & index

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const validator = require('express-validator');
 const contactListRouter = require('../contact')
 const app = express()
 
@@ -7,9 +8,26 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
-
+app.use(validator());
 app.use('/',contactListRouter)
 
+exports.showdata = function (req,res){
+    req.checkBody('email', 'email is required.').notEmpty();
+    req.checkBody('firstname', 'firstname is required.').notEmpty();
+    req.checkBody('lastname', 'lastname is required.').notEmpty();
 
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.send(errors);
+        return;
+    } else{
+        res.json({
+            email: req.body.email,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        });
+    }
+};
 
 module.exports = app
